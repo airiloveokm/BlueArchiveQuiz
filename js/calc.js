7edf62a4d4e28cbd78dafa2c7c45bdf2dc2c64d7
okomebl11754 <48635039+airiloveokm@users.noreply.github.com>
2024-10-14 18:51:06 +0900

    Add files via upload
    アズサの身長データを修正

--- js/calc.js
+++ js/calc.js
@@ -126,7 +126,7 @@ function loadJsonFile() {
         }
     };
     //HTTPメソッドとアクセスするサーバーのURLを指定
-    req.open("GET", "./data/profile.json", false);
+    req.open("GET", "./data/profile.json?202410140852", false);
     //実際にサーバーへリクエストを送信
     req.send(null);
 
